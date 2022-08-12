4d62409ddbe991ed9ad3ddf3b14722b8f759b2ee
EvilYep <guillaume.moneret@gmail.com>
2022-08-12 10:37:47 +0200

    bringing it all together :shallow_pan_of_food:

--- js/components/App.js
+++ js/components/App.js
@@ -1,7 +1,13 @@
-import AppButton from "./AppButton.js";
+import Assignments from "./Assignments.js";
 
 export default {
     components: {
-        'app-button': AppButton,
-    }
+        Assignments,
+    },
+
+    template: `
+        <assignments></assignments>
+    `,
+
+    
 };
