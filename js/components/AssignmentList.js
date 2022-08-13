80308a68b8b266d0506eb0268f6911d6c96e604b
EvilYep <guillaume.moneret@gmail.com>
2022-08-13 06:51:33 +0200

    playing around :joystick:

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -7,11 +7,27 @@ export default {
 
     template: `
         <section  v-show="assignments.length">
-            <h2 class="font-bold mb-2">{{ title }}</h2>
+            <h2 class="font-bold mb-2">
+                 {{ title }}
+                 <span>({{ assignments.length }})</span>
+            </h2>
 
-            <ul class="border border-gray-600 divide-y divide-gray-600">
+            <div class="flex gap-2">
+                <button 
+                    v-for="tag in tags" 
+                    @click="currentTag = tag"
+                    class="border rounded px-1 py-px text-xs"
+                    :class="{
+                        'border-blue-500 text-blue-500': tag === currentTag
+                    }"
+                >
+                    {{ tag }}
+                </button>
+            </div>
+
+            <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
                 <assignment
-                    v-for="assignment in assignments"
+                    v-for="assignment in filteredAssignments"
                     :key="assignment.id"
                     :assignment="assignment"   
                 ></assignment>
@@ -22,5 +38,24 @@ export default {
     props: {
         assignments: Array,
         title: String,
+    },
+
+    data() {
+        return {
+            currentTag: 'all',
+        }
+    },
+
+    computed: {
+        tags() {
+            return ['all', ...new Set(this.assignments.map(a => a.tag))];
+        },
+
+        filteredAssignments() {
+            if (this.currentTag === 'all') {
+                return this.assignments;
+            }
+            return this.assignments.filter(a => a.tag === this.currentTag);
+        }
     }
 }
